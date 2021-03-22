8a625201afff42d3585743d0d0b8c485fcab79af
jinxing.liu <821901008@qq.com>
2021-03-22 10:43:23 +0800

    feat: 添加测试

--- channel/work_test.go
+++ channel/work_test.go
@@ -2,7 +2,9 @@ package channel
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -19,6 +21,26 @@ func TestChanel(t *testing.T) {
 	ch <- 1
 
 	fmt.Println("HaHa")
+}
+
+func TestChanelWaitGroup(t *testing.T) {
+	
+	var group sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		group.Add(1)
+
+		// 异步执行
+		go func(num int) {
+			defer group.Done()
+			time.Sleep(time.Second)
+			fmt.Println(num)
+		}(i)
+	}
+
+	// 等待异步执行完成
+	group.Wait()
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "所有事情处理都完了")
 }
 
 func TestDigitsSum(t *testing.T) {
