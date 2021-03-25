cf650d0daaf1b8e37305c9d1b9f294cd3cc70a24
jinxing.liu <821901008@qq.com>
2021-03-25 11:07:46 +0800

    feat: 测试

--- http/client.go
+++ http/client.go
@@ -3,6 +3,8 @@ package http
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func Get(url string) (string, error) {
@@ -21,4 +23,40 @@ func Get(url string) (string, error) {
 	}
 
 	return string(str), nil
+}
+
+func Post(url string) (string, error) {
+
+	// 发送POST请求
+	response, err := http.Post(url, "appliction/json;charset=utf-8", strings.NewReader(`{"username":"jinxing.liu"}`))
+	if err != nil {
+		return "", err
+	}
+
+	defer response.Body.Close()
+
+	str, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(str), nil
+}
+
+func GetUrl(api string, data url.Values) string {
+	query := data.Encode()
+	
+	// 判断不存在? 那么直接返回: http://localhost?username=123
+	if !strings.Contains(api, "?") {
+		return api + "?" + query
+	}
+
+	// 判断最后一个字符串是哪个，如果是?、& 直接返回
+	last := api[len(api)-1:]
+	if last == "?" || last == "&" {
+		return api + query
+	}
+
+	// 剩下 http://localhost?u=123 这种情况需要添加 & 
+	return	api + "&" + query
 }
--- http/client_test.go
+++ http/client_test.go
@@ -3,15 +3,38 @@ package http
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGet(t *testing.T) {
-	str, err := Get("http://localhost:8081?name=123")
+	// 成功
+	str, err := Get("http://local.verystar.net/data/index?name=123")
 	assert.NoError(t, err)
 	data := make(map[string]interface{})
 	assert.NoError(t, json.Unmarshal([]byte(str), &data))
 	fmt.Println(data)
+
+	// 错误
+	_, err = Get("http://localhost:90023")
+	assert.Error(t, err)
+}
+
+func TestPost(t *testing.T) {
+	str, err := Post("http://local.verystar.net/data/index?name=123")
+	assert.NoError(t, err)
+	fmt.Println(str)
+	_, err = Post("http://localhost:90023")
+	assert.Error(t, err)
+}
+
+func TestGetUrl(t *testing.T) {
+	data := url.Values{}
+	data.Add("username", "jinxing.liu")
+	assert.Equal(t, "http://localhost?u=123&username=jinxing.liu", GetUrl("http://localhost?u=123", data))
+	assert.Equal(t, "http://localhost?u=123&username=jinxing.liu", GetUrl("http://localhost?u=123&", data))
+	assert.Equal(t, "http://localhost?username=jinxing.liu", GetUrl("http://localhost", data))
+	assert.Equal(t, "http://localhost?username=jinxing.liu", GetUrl("http://localhost?", data))
 }
